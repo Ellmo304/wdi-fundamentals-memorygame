3fc1a38855da841bb0af9284f3a28977964b25ca
Elliot Mountford-Brock <ell_304@msn.com>
2016-09-18 18:14:42 +0100

    assignment 10.7, elements created

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,34 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+var gameBoard = document.getElementById('game-board');
+
+
+
+
+
+var createBoard = function() {
+
+var boardDiv = getElementById("game-board");
+
+for (var populate = 0; populate < 4; populate +=1){
+	var cardDivs = document.createElement("div");
+	cardDivs.className = ("card");
+	boardDiv.appendChild(cardDivs)
+
+}
+
+createBoard()
+
+
+
+//{if (cardOne === cardTwo) {
+//	alert("Well done, you found a match!");
+//}
+//else if (cardThree === cardFour) { 
+//	alert("Well done, you found a match!");
+//} else  
+//	alert("Sorry, try again.");
+//}
